89cf2307edb56bdfb52d78ec9c96d67720343229
Bregnballe <bregnballe@gmail.com>
2020-09-28 15:21:23 +0200

    basic graphql api

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("Hello there!");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+  `),
+    rootValue: {
+      events: () => {
+        return ["hey", "yo", "hej"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
